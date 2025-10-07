a4bf0925949275b33de4004e0fe044485e1a6165
me <memalhot@redhat.com>
2025-10-07 10:49:21 -0400

    script for testing in the container

--- bt-test.sh
+++ bt-test.sh
@@ -1,44 +1,44 @@
-echo "testing bj valid commands"
-python3 batchtools.py bj -h
-python3 batchtools.py bj --help
-# python3 batchtools.py bj -w
-# python3 batchtools.py bj --watch
+# echo "testing bj valid commands"
+# python3 batchtools.py bj -h
+# python3 batchtools.py bj --help
+# # python3 batchtools.py bj -w
+# # python3 batchtools.py bj --watch
 
-echo "testing bj invalid commands"
-python3 batchtools.py bj -z
-
-
-echo "testing bd valid commands"
-python3 batchtools.py bd -h
-python3 batchtools.py bd --help
-br nvidia-smi
-python3 batchtools.py bd
-#NEED TO ADD CASE FOR SPECIFIED WORKLOAD
-
-echo "testing bj invalid commands"
-python3 batchtools.py bd -z
-python3 batchtools.py bd thisisafakejob
+# echo "testing bj invalid commands"
+# python3 batchtools.py bj -z
 
 
-echo "testing bp valid commands"
-python3 batchtools.py bp -h
-python3 batchtools.py bp --help
-python3 batchtools.py bp
-python3 batchtools.py bp csw-dev-0
+# echo "testing bd valid commands"
+# python3 batchtools.py bd -h
+# python3 batchtools.py bd --help
+# br nvidia-smi
+# python3 batchtools.py bd
+# #NEED TO ADD CASE FOR SPECIFIED WORKLOAD
+
+# echo "testing bj invalid commands"
+# python3 batchtools.py bd -z
+# python3 batchtools.py bd thisisafakejob
+
+
+echo "testing bl valid commands"
+python3 batchtools.py bl -h
+python3 batchtools.py bl --help
+python3 batchtools.py bl
+python3 batchtools.py bl csw-dev-0
 #NEED TO ADD CASE WITH MULTIPLE PODS
 
-echo "testing bj invalid commands"
-python3 batchtools.py bd -z
-python3 batchtools.py bd thisisafakepod
+echo "testing bl invalid commands"
+python3 batchtools.py bl -z
+python3 batchtools.py bl thisisafakepod
 
-echo "testing bq valid commands"
-python3 batchtools.py bq -h
-python3 batchtools.py bq --help
-python3 batchtools.py bq
+# echo "testing bq valid commands"
+# python3 batchtools.py bq -h
+# python3 batchtools.py bq --help
+# python3 batchtools.py bq
 
-echo "testing bj invalid commands"
-python3 batchtools.py bq randomnewarg
-python3 batchtools.py bq -z
+# echo "testing bj invalid commands"
+# python3 batchtools.py bq randomnewarg
+# python3 batchtools.py bq -z
 
 
 
