e150058af4902f3dd43a5660e8674e8fb4a47fca
me <memalhot@redhat.com>
2025-10-16 08:37:37 -0400

    cleanup

--- batchtools.py
+++ batchtools.py
@@ -201,7 +201,6 @@ def bp(job_names: list[str] | None = None) -> int:
 
 def bq(args) -> int:
     try:
-        with oc.tracking() as t:
             clusterqueues = oc.selector("clusterqueue").objects()
             if not clusterqueues:
                 print("No ClusterQueues found.")
