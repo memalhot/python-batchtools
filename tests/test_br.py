f5ea1ea13b8e598ec2b291d8d2b8700b64497bdd
Lars Kellogg-Stedman <lars@redhat.com>
2025-10-31 08:13:59 -0400

    Parametrize test_create_job_nowait
    Use pytest's support for parametrized tests to test the behavior of
    br.pytes with both gpu=v100 and gpu=none. This lets us validate that the
    resources section of the job spec changes as expected.

--- tests/test_br.py
+++ tests/test_br.py
@@ -24,6 +24,25 @@ def test_invalid_gpu(args: argparse.Namespace):
     assert "ERROR: unsupported GPU invalid" in err.value.code
 
 
+@pytest.mark.parametrize(
+    "gpu, resources",
+    [
+        (
+            "v100",
+            {
+                "requests": {"nvidia.com/gpu": "1"},
+                "limits": {"nvidia.com/gpu": "1"},
+            },
+        ),
+        (
+            "none",
+            {
+                "requests": {"cpu": "1", "memory": "1Gi"},
+                "limits": {"cpu": "1", "memory": "1Gi"},
+            },
+        ),
+    ],
+)
 @mock.patch("openshift_client.create", name="create")
 @mock.patch("openshift_client.selector", name="selector")
 @mock.patch("socket.gethostname", name="gethostname")
@@ -33,6 +52,8 @@ def test_create_job_nowait(
     mock_gethostname,
     mock_selector,
     mock_create,
+    gpu,
+    resources,
     args: argparse.Namespace,
     tempdir,
     parser,
@@ -41,8 +62,12 @@ def test_create_job_nowait(
     CreateJobCommand.build_parser(subparsers)
     args = parser.parse_args(["br"])
     args.wait = False
-    args.job_id = 123
+    args.job_id = "test"
+    args.image = "test-image"
+    args.gpu = gpu
     args.command = ["true"]
+
+    queue_name = "dummy-localqueue" if gpu == "none" else f"{gpu}-localqueue"
 
     mock_getcwd.return_value = tempdir
     mock_gethostname.return_value = "testhost"
@@ -64,9 +89,9 @@ def test_create_job_nowait(
         "apiVersion": "batch/v1",
         "kind": "Job",
         "metadata": {
-            "name": "job-v100-123",
+            "name": f"job-{gpu}-test",
             "labels": {
-                "kueue.x-k8s.io/queue-name": "v100-localqueue",
+                "kueue.x-k8s.io/queue-name": queue_name,
                 "test_name": "kueue_test",
             },
         },
@@ -80,17 +105,14 @@ def test_create_job_nowait(
                     "restartPolicy": "Never",
                     "containers": [
                         {
-                            "name": "job-v100-123-container",
                             "command": [
                                 "/bin/bash",
                                 "-c",
                                 f"testcommand {' '.join(args.command).strip()}",
                             ],
-                            "image": "image-registry.openshift-image-registry.svc:5000/redhat-ods-applications/csw-run-f25:latest",
-                            "resources": {
-                                "requests": {"nvidia.com/gpu": "1"},
-                                "limits": {"nvidia.com/gpu": "1"},
-                            },
+                            "name": f"job-{gpu}-test-container",
+                            "image": "test-image",
+                            "resources": resources,
                         }
                     ],
                 }
